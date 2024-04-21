e695f9109a07f129aeb42dc94ca48dd64bdea947
MahmoudSharqawi <mahmmoudmohamed2020@gmail.com>
2024-04-22 00:32:18 +0200

    Fix:Enhance Validator

--- src/Validators/InvoiceValidator.php
+++ src/Validators/InvoiceValidator.php
@@ -6,40 +6,34 @@ use DateTime;
 
 class InvoiceValidator implements Validator
 {
-    private array $row;
     private array $validated = [];
 
-    public function __construct(array $row)
-    {
-        $this->row = $row;
-    }
-
-    public function validate(): array
+    public function validate(array $row): array
     {
         $keys = [0 => 'invoice', 1 => 'invoice date', 8 => 'grand total'];
 
         foreach ($keys as $index => $fieldName) {
-            if (!isset($this->row[$index])) {
+            if (!isset($row[$index])) {
                 throw new \InvalidArgumentException("Missing field '$fieldName' in the row.");
             }
 
             switch ($fieldName) {
                 case 'invoice':
-                    if (!is_numeric($this->row[$index])) {
+                    if (!is_numeric($row[$index])) {
                         throw new \InvalidArgumentException("Invalid invoice number format.");
                     }
-                    $this->validated['id'] = (int)$this->row[$index];
+                    $this->validated['id'] = (int)$row[$index];
                     break;
                 case 'invoice date':
-                    $invoiceDate = $this->daysToDate($this->row[$index]);
+                    $invoiceDate = $this->daysToDate($row[$index]);
 //                    var_dump($this->row,$invoiceDate);die;
                     $this->validated['date'] = $invoiceDate;
                     break;
                 case 'grand total':
-                    if (!is_numeric($this->row[$index])) {
+                    if (!is_numeric($row[$index])) {
                         throw new \InvalidArgumentException("Invalid grand total format.");
                     }
-                    $this->validated['grand_total'] = (float)$this->row[$index];
+                    $this->validated['grand_total'] = (float)$row[$index];
                     break;
             }
         }
